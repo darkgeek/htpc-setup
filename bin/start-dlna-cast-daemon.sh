e65011e05ef887002c336943d1fc51db01565db0
Justin Yang <linuxjustin@gmail.com>
2025-01-12 13:53:39 +0800

    No need proxy anymore

--- bin/start-dlna-cast-daemon.sh
+++ bin/start-dlna-cast-daemon.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 pkill -9 -f mpv
 pkill -9 -f macast
 export https_proxy=http://localhost:8118
 export http_proxy=http://localhost:8118
-/home/justin/.local/bin/macast-cli
+macast-cli
--- bin/start-poor-man-web-cast.sh
+++ bin/start-poor-man-web-cast.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
 pkill -9 -f cast-receiver
-export https_proxy=http://localhost:8118
-export http_proxy=http://localhost:8118
 cd /home/justin/Shares/poor-man-web-cast && /usr/bin/go run .
